4305983441103033aa8774aad5f46284e3d200f1
unknown <unknown@example.com>
2023-03-12 21:06:38 +0800

    基本框架搭建

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,24 @@
 // index.js
 
 Page({
+  onReady:function(){
+    this.animation = wx.createAnimation({
+      duration:100,
+      timingFunction:"ease"
+    })
+  },
   data: {
-
+    y:0
+  },
+  play_animation:function(){
+    this.animation.rotate(10).step().rotate(-10).step().rotate(10).step().rotate(-10).step().rotate(10).step().rotate(-10).step().rotate(0).step()
+    this.setData({animation: this.animation.export()})
+  },
+  shake:function(){
+    this.play_animation();
+    this.setData({
+      y:500
+    })
   },
 
 })
